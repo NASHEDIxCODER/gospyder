292bf5c8389da343fab08d4e78c1df5e6f7220c2
sonu kumar <psycho01king@gmail.com>
2025-11-24 09:24:20 +0000

    test

--- pkg/enum/brute.go
+++ pkg/enum/brute.go
@@ -1,1 +1,57 @@
 package enum
+
+import (
+	"bufio"
+	"context"
+	"gospyder/pkg/models"
+	"gospyder/pkg/resolver"
+	"os"
+	"strings"
+	"sync"
+)
+
+// bruteforce performs parallel DNS brute-forcing
+
+func BruteForce (ctx context.Context, pool *resolver.Pool, target string, wordlist string)(<-chan models.Domain, error){
+	out:= make(chan models.Domain, 100)
+	 file, err := os.Open(wordlist)
+	if err != nil {
+		return nil, err
+	}
+	go func() {
+		defer close (out)
+		defer file.Close()
+		scanner := bufio.NewScanner(file)
+		var wg sync.WaitGroup
+		sem := make(chan struct{}, 500) //500 concurrent brute-force attemps 
+		
+		for scanner.Scan(){
+			select{
+			case <-ctx.Done():
+				return 
+			default:
+			}
+			subdomain := strings.TrimSpace(scanner.Text())
+			if subdomain == ""{
+				continue
+			}
+			fullDomain := subdomain+ "." + target
+			wg.Add(1)
+			go func (domain string){
+				defer wg.Done()
+				sem <-struct{}{}
+				defer func () {<-sem }()
+				ips, err := pool.Lookup(ctx, domain)
+				if err == nil && len(ips) > 0{
+					out <- models.Domain{
+						Name: domain,
+						Source: "brute",
+						Found: ips,
+					}
+				}
+			}(fullDomain)
+		}
+		wg.Wait()
+	}()
+	return out, nil
+}
--- pkg/enum/recursive.go
+++ pkg/enum/recursive.go
@@ -1,1 +1,38 @@
 package enum
+
+import (
+	"context"
+	// "gospyder/pkg/models"
+	"gospyder/pkg/resolver"
+	"strings"
+)
+
+func Recursive(ctx context.Context, pool *resolver.Pool, foundDomain string)(<-chan string, error) {
+	out := make(chan string, 50)
+
+	go func () {
+		defer close(out)
+
+		base := strings.Split(foundDomain, ".")[0]
+		suffix := strings.Join(strings.Split(foundDomain, ".")[1:], ".")
+
+		permutations := []string {
+			base + "1",
+			base +"2",
+			"dev-" +base,
+			"test-" +base,
+			base + "-admin",
+			"admin-" + base,
+		}
+
+		for _, perm := range permutations {
+			select {
+			case <-ctx.Done():
+				return 
+			default:
+			}
+			out <- perm + "." + suffix
+		}
+	}()
+	return out, nil
+}
